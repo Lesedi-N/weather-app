945365f227fbac5db947f547316491b9b9af781a
Lesedi-N <127985869+Lesedi-N@users.noreply.github.com>
2023-03-18 17:21:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function currentDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if ( hours < 10 ) {
+hours = 0 + hours;
+}
+let minutes = date.getMinutes();
+if ( minutes < 10){
+    minutes = 0 + minutes;
+}
+let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let day = days[date.getDay()];
+return day + " " + hours + ":" + minutes;
+
+}
+
 function displayTemp(response) {
     console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
@@ -10,11 +26,13 @@ let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML = response.data.main.humidity;
 let windElement = document.querySelector("#wind");
 windElement.innerHTML = Math.round(response.data.wind.speed);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = currentDate(response.data.dt*1000) ;
 }
 
 let apiKey = "e5d59a8b6235a1f821f95b35ab5b2c07";
 let apiUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q=London&appid=e5d59a8b6235a1f821f95b35ab5b2c07&units=metric";
+  "http://api.openweathermap.org/data/2.5/weather?q=Protea Glen&appid=e5d59a8b6235a1f821f95b35ab5b2c07&units=metric";
 
 
 axios.get(apiUrl).then(displayTemp);
